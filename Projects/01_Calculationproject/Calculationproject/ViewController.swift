d966bec4ab3b327342db63a2e4e2cf936179604c
joeseonmi <jemma3136@gmail.com>
2017-05-11 18:31:02 +0900

    error

--- Projects/01_Calculationproject/Calculationproject/ViewController.swift
+++ Projects/01_Calculationproject/Calculationproject/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var dispNum: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func num1(_ sender: UIButton) {
+        print("1")
+    }
+    @IBAction func num2(_ sender: UIButton) {
+    }
+    @IBAction func num3(_ sender: UIButton) {
+    }
+    @IBAction func num4(_ sender: UIButton) {
+    }
+    @IBAction func num5(_ sender: UIButton) {
+    }
+    @IBAction func num6(_ sender: UIButton) {
+    }
+    @IBAction func num7(_ sender: UIButton) {
+    }
+    @IBAction func num8(_ sender: UIButton) {
+    }
+    @IBAction func num9(_ sender: UIButton) {
+    }
+    @IBAction func num0(_ sender: UIButton) {
+    }
+    @IBAction func dot(_ sender: UIButton) {
+    }
+    @IBAction func add(_ sender: UIButton) {
+    }
+    @IBAction func equal(_ sender: UIButton) {
+    }
+    @IBAction func mi(_ sender: UIButton) {
+    }
 
+    
+    
 }
 
