8524a619ac5a73316deb5e90b646b63b7ca47682
joeseonmi <jemma3136@gmail.com>
2017-05-14 18:33:01 +0900

    edit playgroundfile

--- Study/04_if문/exPractice2.playground/Contents.swift
+++ Study/04_if문/exPractice2.playground/Contents.swift
@@ -38,7 +38,7 @@ func getGrade(inScore score:UInt) -> String
         }
     }
     return getGrade
-    //일단 겟그레이드는 "F" 인데 각 조건들이 맞으면 그걸로 출력하고 정 없으면 "F" 출력한다는 의미인가
+
 }
 
 getGrade(inScore: 98)
@@ -78,18 +78,17 @@ func getPoint(inGrade grade:String) -> Double
 }
 
 getPoint(inGrade: "A+")
-//이 함수를 만들때 걸렸던거 => 아까는 let으로 선언했는데 지금은 var로 선언해줌(겟포인트의 값이 계속 바뀌기때문)
+
 
 //점수를 받아서 포인트로 반환하는 함수
 
 func getPointFromScore(inScore score:Int) -> Double
 {
     return getPoint(inGrade: getGrade(inScore: UInt(score)))
-    //특히 마지막줄에 타입안쓰고 score괄호빼고 넣었는데 반환안되고, 괄호넣고 타입넣으니까 되는데 이유가뭐지
 }
 
 
 getPointFromScore(inScore: 95)
 
-//리턴값넣는거 어려워따 함수를 이해못한건지 변수를 이해못한건지도 모르겠다씌바
+
 
